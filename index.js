7ec0c20f43fc3ec45786304e678495ae342f8740
Christian Johannesson <christian.johannesson@drakryggen.com>
2021-05-20 09:24:03 +0200

    Added to index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
@@ -19,11 +20,13 @@ app.use("/api", router);
 
 const PORT = process.env.PORT || 8080;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-}
+//set up static folder for rendering react app
+app.use(express.static(path.join(__dirname, "frontend/build")));
 
-app.use(express.static("frontend/build"));
+//Handle any requests that doesn't target root url
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`server running on port: ${PORT}`);
